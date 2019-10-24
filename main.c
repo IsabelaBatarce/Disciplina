bb47fa582a6dcec74aa0e6399186d0a616ae1e30
SamanthaBeatriz19 <49728936+SamanthaBeatriz19@users.noreply.github.com>
2019-10-24 17:18:37 -0300

    quinta

--- main.c
+++ main.c
@@ -2,7 +2,12 @@
 #include <string.h>
 #include <stdio.h>
 #include "aluno.h"
+#include "DISP.h"
+
 int main(){
-	
+	char busca[100];	
+	printf("Digite o codigo da disciplina: ");
+	scanf("%s", busca);
+	ConsultaDisciplina(busca);
 	return 0;
 }
