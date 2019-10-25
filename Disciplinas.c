561a2c0f0bcb56c7f6a8b9d3dfe6ab670204e010
SamanthaBeatriz19 <49728936+SamanthaBeatriz19@users.noreply.github.com>
2019-10-25 20:38:13 -0300

    atualizado
    login ok
    pesquisar disciplinas com pre req ok
    cadastro n ta ok

--- Disciplinas.c
+++ Disciplinas.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include "DISP.h"
-void ConsultaDisciplina(char cod[5]){
+void ConsultaDisciplina(char cod[6]){
 	FILE * fp = fopen("Disciplinas.txt", "r");
 	const char s[2] = ",";
 	char *token;
@@ -14,21 +14,21 @@ void ConsultaDisciplina(char cod[5]){
 		token = strtok(linha, s);
 		for(int b=0;b<3;b++) {
         	if(b==0){
-				char sub[6];
-				for(int ee=0;ee<5;ee++){
-					sub[ee] = token[ee];
-				}
-				strcpy(aux->codigo,sub);
+			char sub[6];
+			for(int ee=0;ee<5;ee++){
+				sub[ee] = token[ee];
+			}
+			strcpy(aux->codigo,sub);
 				//printf( "%s\n", aux->codigo);
-			}
-    		if(b==1){
-				strcpy(aux->nome,token);
-				//printf( "%s\n", aux->nome);
-			}
-			if(b==2){
-				aux->creditos= token[0] - 48;
-				//printf( "%d\n", aux->creditos);
-			}
+		}
+		if(b==1){
+			strcpy(aux->nome,token);
+			//printf( "%s\n", aux->nome);
+		}
+		if(b==2){
+			aux->creditos= token[0] - 48;
+			//printf( "%d\n", aux->creditos);
+		}
     		token = strtok(NULL, s);
    		}
 		DISP.p[a]=aux;	
@@ -40,13 +40,27 @@ void ConsultaDisciplina(char cod[5]){
 			printf("nome:%s\ncreditos:%d\n",DISP.p[a]->nome,DISP.p[a]->creditos);
 	}
 	//pre requisitos
-	/*fp = fopen("prerequisitos.txt","r");
+	fp = fopen("prerequisitos.txt","r");
 	for(int a=0; a<31;a++){
 		fgets(linha, 100, fp);
 		token = strtok(linha, s);
-		printf("pre requisitos: %s",token);
+		int test=0;
+		for(int b=0;b<2;b++) {
+			if(b==0){
+				if(strcmp (token, cod) == 0){
+					test=1;
+				}
+				//printf("tok =%s\ncod = %s\n",token,cod);
+			}
+			if((b==1)&&(test==1)){
+				printf("Pre requisito = %s",token);
+				test=0;
+			}
+			token = strtok(NULL, s);
+			
+		}
 	}
-	fclose(fp);*/
+	fclose(fp);
 	
 }
 
@@ -86,10 +100,14 @@ Aluno* newAluno(char *ra, char *nome, char *log, char *sen){
 int loginAluno(Alunos *x){
 	FILE * fp = fopen("Alunos.txt","r");
 	char login[100],senha[100];
+	printf("***ENTRAR***\n");
 	printf("Login:\n");
 	fgets(login,1000,stdin);
 	printf("Senha:\n");
 	fgets(senha,1000,stdin);
+	login[strlen(login)-1]='\0';
+	senha[strlen(senha)-1]='\0';
+	//printf("-%s-",login);
 	char ra[10],nome[100],log[100],sen[100];
 	for(int i=0;i<x->top;i++){
 		fscanf(fp,"%s\n",ra);		
@@ -99,6 +117,14 @@ int loginAluno(Alunos *x){
 //eu n sei
 		x->a[i]=newAluno(ra,nome,log,sen);	
 	}
-	//printf("%s %s %s %s",x->a[0]->ra,x->a[0]->nome,x->a[0]->login,x->a[0]->senha);
-	return 0;
+	//printf("-%s -%s -%s -%s\n",login,senha,x->a[0]->login,x->a[0]->senha);
+	for(int i=0;i<x->top;i++){
+			
+		if(strcmp (x->a[0]->login, login) == 0){
+			if(strcmp (x->a[0]->senha, senha) == 0){
+				return 0;
+			}	
+		}
+	}
+	return 1;
 } 
